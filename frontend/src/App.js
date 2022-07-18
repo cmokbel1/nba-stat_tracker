9d1930a51a2dc048feec421c66a780704c272435
Claude Mokbel <claudemokbel@gmail.com>
2022-07-17 17:59:06 -0700

    project boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <h1>Welcome to the NBA Stat Tracker</h1>
+      </header>
+      <body>
+        <h1>Website Under Construction</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
