59d3bfa22ef6c80c278efd3bc0e930e195bc7e99
Claude Mokbel <claudemokbel@gmail.com>
2022-07-22 08:09:26 -0700

    addressed PR

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,16 @@ app.get('/teams/conference/:conference', async (req, res) => {
   const data = await rapidapi.teams.getTeamsByConference(req.params.conference);
   if (data.error) {
     res.status(400);
-  } else {
-    res.json(data);
   }
+    res.json(data);
 });
 
 app.get('/teams/:id', async (req, res) => {
   const data = await rapidapi.teams.getTeamById(req.params.id);
   if (data.error) {
     res.status(400);
-  } else {
-    res.json(data);
   }
+    res.json(data);
 })
 // get all players on a team for current season
 app.get('/teams/:id/players', async (req, res) => {
@@ -33,18 +31,16 @@ app.get('/teams/:id/players', async (req, res) => {
   const data = await rapidapi.teams.getTeamPlayersBySeason(req.params.id, season);
   if (data.error) {
     res.status(400);
-  } else {
-    res.json(data);
   }
+    res.json(data);
 })
 
 app.get('/teams/division/:division', async (req,res) => {
   const data = await rapidapi.teams.getTeamsByDivision(req.params.division);
   if (data.error) {
     res.status(400);
-  } else {
-    res.json(data);
   }
+    res.json(data);
 })
 
 app.get('teams/:id/statistics', async (req,res) => {
@@ -52,18 +48,16 @@ app.get('teams/:id/statistics', async (req,res) => {
   const data = await rapidapi.teams.getTeamStatisticsById(req.params.id, season);
   if (data.error) {
     res.status(400);
-  } else {
-    res.json(data);
   }
+    res.json(data);
 })
 
 app.get('/players/:id', async (req, res) => {
   const data = await rapidapi.players.getPlayerById(req.params.id);
   if (data.error) {
     res.status(400);
-  } else {
-    res.json(data);
   }
+    res.json(data);
 })
 
 app.listen(port, () => {
