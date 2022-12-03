5225d75546895f056b89fdf39b9d9aff4998b20a
Claude Mokbel <claudemokbel@gmail.com>
2022-12-02 20:54:31 -0800

    add class to nav Link, add styles for team cards, nav bar, and import new font

--- frontend/src/components/teamListCard.js
+++ frontend/src/components/teamListCard.js
@@ -6,7 +6,6 @@ export const TeamListCard = ({ team, index, setError }) => {
         <>
             <li key={index} className="team-list-card">
                 <Link to={`/team/${team.id}`} style={{ textDecoration: 'none'}}>
-                    <h3>{team.name}</h3>
                     <img style={{ height: '150px', width: '150px' }} src={team.logo} alt="logo" />
                 </Link>
             </li>
